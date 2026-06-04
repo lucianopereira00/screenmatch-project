9e2bc2f1f844ba3f13a8fb52cf956e538e125e42
Luciano <lucianosccp2024@gmail.com>
2026-06-04 16:25:28 -0300

    Atualizando busca de temporadas e episódios

--- src/main/java/br/com/alura/screenmatch/Model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch/Model/DadosEpisodio.java
@@ -1,4 +1,13 @@
 package br.com.alura.screenmatch.Model;
 
-public record DadosEpisodio() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Season")  int season,
+                            @JsonAlias("Episode") int numeroEpisodio,
+                            @JsonAlias("Runtime") String tempoDuracao,
+                            @JsonAlias("Released")String dataLancamento,
+                            @JsonAlias("Genre") String genero){
 }
--- src/main/java/br/com/alura/screenmatch/Model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/Model/DadosTemporada.java
@@ -1,4 +1,10 @@
 package br.com.alura.screenmatch.Model;
 
-public record DadosTemporada() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season")int numeroTemporas,
+                             @JsonAlias("Episodes")List<DadosEpisodio> episodios) {
 }
